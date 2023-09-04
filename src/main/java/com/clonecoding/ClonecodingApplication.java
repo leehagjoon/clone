6a8b15ed0aaf3d139195792d6199f1de58963a5b
leehagjoon(이학준) <winner__loser@naver.com>
2023-09-04 16:33:16 +0900

    init commit

---
+++ src/main/java/com/clonecoding/ClonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonecodingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/clonecoding/config/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package com.clonecoding.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * packageName    : com.clonecoding.config
+ * fileName       : DatabaseConfig
+ * author         : hagjoon
+ * date           : 2023-09-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-09-04        hagjoon       최초 생성
+ */
+@Configuration
+@PropertySource("classpath:/application.yml")
+public class DatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+}
