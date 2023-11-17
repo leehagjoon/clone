60da85381ca39fe0d78670bc7bb3f4580ab54ffc
leehagjoon(이학준) <winner__loser@naver.com>
2023-11-17 17:53:50 +0900

    [회원가입] entity 수정

--- src/main/java/com/clonecoding/dev/jpa/entity/Member.java
+++ src/main/java/com/clonecoding/dev/jpa/entity/Member.java
@@ -23,7 +23,6 @@ import java.time.LocalDateTime;
 @Table(name = "member")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
 public class Member {
 
     @Id
@@ -31,44 +30,58 @@ public class Member {
     @Column(name = "MEMBER_SNO")
     private Long memberSno;
 
-    @Column(name = "MEMBER_ID", nullable = false)
+    @Column(name = "MEMBER_ID")
     private String memberId;
 
-    @Column(name = "MEMBER_PW", nullable = false)
+    @Column(name = "MEMBER_PW")
     private String memberPw;
 
-    @Column(name = "MEMBER_NAME", nullable = false)
+    @Column(name = "MEMBER_NAME")
     private String memberName;
 
-    @Column(name = "BIRTH_DAY", nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "BIRTH_DAY")
     private LocalDateTime birthDay;
 
-    @Column(name = "NICK_NAME", nullable = false)
+    @Column(name = "NICK_NAME")
     private String nickName;
 
-    @Column(name = "HPNO", nullable = false)
+    @Column(name = "HPNO")
     private String hpNo;
 
-    @Column(name = "MEMBER_STATUS_CD", nullable = false)
+    @Column(name = "MEMBER_STATUS_CD")
     private String memberStatusCd;
 
-    @Column(name = "JOIN_DT", nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "JOIN_DT")
     private LocalDateTime joinDt;
 
-    @Column(name = "GENDER_CD", nullable = false)
+    @Column(name = "GENDER_CD")
     private String genderCd;
 
     @Column(name = "PW_UPDT_DT")
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime pwUpdtDt;
 
     @Column(name = "UPDT_DT")
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updtDt;
 
     @Column(name = "MEMBR_AUTH")
     private String membrAuth;
 
+    @Builder
+    public Member(Long memberSno, String memberId, String memberPw, String memberName, LocalDateTime birthDay,
+                  String nickName, String hpNo, String memberStatusCd, LocalDateTime joinDt, String genderCd,
+                  LocalDateTime pwUpdtDt, LocalDateTime updtDt, String membrAuth) {
+        this.memberSno = memberSno;
+        this.memberId = memberId;
+        this.memberPw = memberPw;
+        this.memberName = memberName;
+        this.birthDay = birthDay;
+        this.nickName = nickName;
+        this.hpNo = hpNo;
+        this.memberStatusCd = memberStatusCd;
+        this.joinDt = joinDt;
+        this.genderCd = genderCd;
+        this.pwUpdtDt = pwUpdtDt;
+        this.updtDt = updtDt;
+        this.membrAuth = membrAuth;
+    }
 }
